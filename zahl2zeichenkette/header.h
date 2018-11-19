2fad4f5e52f3a946c00991d0e219da6d8b8f4a39
Mulham-Alesali <MB3.EMAIL@GMAIL.COM>
2018-11-19 20:17:59 +0100

    Create header.h

---
+++ zahl2zeichenkette/header.h
@@ -0,0 +1,7 @@
+#pragma once
+int stringLength(const char * str);
+int strcomp(const char * string1, const char * string2);
+void upper_string(char s[]);
+void lower_string(char s[]);
+char * zahl2zeichenkette(int z, char * zk, int nzk);
+int zeichenkette2zahl(char * zk);
